efdafb9af793c6b86c7e853478c0c020ddfbe374
Quang Anh <qu.anh261299@gmail.com>
2021-04-22 14:20:28 +0700

    xyz

--- src/screens/Detail.js
+++ src/screens/Detail.js
@@ -1,10 +1,59 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, FlatList, Image, StyleSheet } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const DATA = [
+  {
+    photo: 'https://www.forever21.com/dw/image/v2/BFKH_PRD/on/demandware.static/-/Sites-f21-master-catalog/default/dw91f2d4aa/1_front_750/00430045-03.jpg?sw=400&sh=600',
+  }
+]
 
 export default function Detail() {
+    const renderItem = ({ item }) => (
+        <View style={{ width: '100%', }}>
+            <Image
+        style={styles.imgStyle}
+        source={{ uri: item.photo, }}
+        />
+        </View>
+    );
     return (
         <View>
-            <Text>Detail</Text>
+          <FlatList
+            data={DATA}
+            numColumns={2}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+            columnWrapperStyle={{ justifyContent: 'space-around', marginBottom: 20, flex: 1 }}
+            style={{ marginBottom: 15 }}
+          />
+          <View style={{ flexDirection: 'row', borderBottomWidth: 1, borderBottomColor: 'white' }}>
+          <Ionicons name="ios-ellipse-outline" size={40} color="black" />
+        <TouchableOpacity style={styles.a}>
+          <Text style={styles.b}>ADD TO CART</Text>
+        </TouchableOpacity>
+        <Ionicons name="heart-outline" size={40} color="White" />
         </View>
-    )
-}
+        </View>
+      )
+    }
+    
+    const styles = StyleSheet.create({
+      imgStyle: {
+        height: 550,
+        width: 'auto',
+      },
+      a:{ 
+        flex:1,
+        justifyContent: 'center', 
+        alignItems: 'center',
+        backgroundColor:'black',
+        width:220,
+        height:50,
+        marginLeft:20,
+        marginRight:20
+      },
+      b:{
+          color:'#ffffff'
+      }
+    });
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, FlatList, Image, StyleSheet, TextInput } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function Home() {
     return (
         <View>
-            <Text>Home</Text>
+        <View style ={{flexDirection:'row'}}>
+            <Ionicons name ="ios-reorder-three-outline" size={35} color="black" />  
+              <Text style = {styles.a}>FOREVER 21</Text>
+        </View>
+            <TextInput style ={{flexDirection:'row', borderWidth:1, marginTop:10, marginLeft:10, marginRight:10}}
+            placeholder='Search'>
+                <Ionicons name ="ios-search" size={35} color="black" />  
+            </TextInput>
         </View>
     )
 }
+const styles = StyleSheet.create({
+    a:{
+       
+        textAlignVertical:'center',
+        marginLeft:100,
+        fontSize:20   
+    }
+})
